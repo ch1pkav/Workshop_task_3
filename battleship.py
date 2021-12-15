62d61e25a08f77c740337203818e5451b43f5ded
orplazh <vshuliar01@gmail.com>
2021-12-15 12:25:17 +0200

    vlad

--- battleship.py
+++ battleship.py
@@ -40,7 +40,7 @@ def board_generator():
     return field
 
 
-def put_ships():
+def put_ships(board: list):
     """
     player puts their ships
     """
@@ -50,22 +50,96 @@ Now you are to put your ships on the field.
 You are provided with 4 ships:
 
 The first ship size is 1*3:
-+
-+
-+
+B
+B
+B
 
 The second ship size is 2*1:
-++
+B
+B
 
 The third ship size is 1*1:
-+
+B
 
 The fourth ship size is 1*1:
-+
+B
 """)
 
-    coord_col = input("Choose column coord for your first ship (0-4): ")
-    row_col = input("Choose row coord for your first ship (0-4): ")
+    board = first_ship(board)
+    board = second_ship(board)
+    board = small_ship(board)
+    board = small_ship(board)
+
+    return board
+
+
+def first_ship(board: list):
+    """
+    first ship 1*3
+    """
+
+    row = int(input("Choose left row coord for your first ship (0-4): "))
+    col = int(input("Choose top column coord for your first ship (0-4): "))
+    l_ship = 3
+    values = [0, 1, 2, 3, 4]
+    if row not in values or col not in values or l_ship + row > 4:
+        print('Wrong coordinates, try again')
+        return first_ship(board)
+
+    for i in range(5):
+        for j in range(5):
+            if j == row and i == col and l_ship > 0:
+                board[row][col] = 'B'
+                row += 1
+                l_ship -= 1
+
+    return board
+
+
+def second_ship(board: list):
+    """
+    second ship 1*2
+    """
+
+    row = int(input("Choose left row coord for your second ship (0-4): "))
+    col = int(input("Choose top column coord for your second ship (0-4): "))
+    l_ship = 2
+    values = [0, 1, 2, 3, 4]
+    if row not in values or col not in values or l_ship + row > 4:
+        print('Wrong coordinates, try again')
+        return second_ship(board)
+
+    for i in range(5):
+        for j in range(5):
+            if j == row and i == col and l_ship > 0:
+                board[row][col] = 'B'
+                row += 1
+                l_ship -= 1
+
+    return board
+
+
+def small_ship(board: list):
+    """
+    third and fourth ship
+    """
+
+    row = int(input("Choose left row coord for your small ship (0-4): "))
+    col = int(input("Choose top column coord for your small ship (0-4): "))
+    values = [0, 1, 2, 3, 4]
+    if row not in values or col not in values or board[row][col] == 'B':
+        print('Wrong coordinates, try again')
+        return small_ship(board)
+
+    for i in range(5):
+        for j in range(5):
+            if j == row and i == col:
+                board[row][col] = 'B'
+
+    return board
+
+
+def ship_putter(board: list):
 
 
 def shot(board: list):
